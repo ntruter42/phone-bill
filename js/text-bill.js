144e56ece684b282c9fc378d2acc9e0373d46765
Nicholas Truter <ntruter42@gmail.com>
2023-04-09 15:18:43 +0200

    Update text-bill.js - created functioning text input bill widget

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,12 +1,39 @@
-// get a reference to the textbox where the bill type is to be entered
+const billTypeText = document.querySelector('.billTypeText');
+const addToBillBtn = document.querySelector('.addToBillBtn');
+const callsTotalElem = document.querySelector('.callTotalOne');
+const smsTotalElem = document.querySelector('.smsTotalOne');
+const totalCostElem = document.querySelector('.totalOne');
+const resetBtn = document.querySelector('.textBillResetBtn');
 
-//get a reference to the add button
+let callsTotal = 0;
+let smsTotal = 0;
 
-//create a variable that will keep track of the total bill
+function textBillTotal() {
+	const billTypeEntered = billTypeText.value.trim();
+	if (billTypeEntered === 'call') {
+		callsTotal += 2.75;
+	} else if (billTypeEntered === 'sms') {
+		smsTotal += 0.75;
+	}
+	callsTotalElem.innerHTML = "R" + callsTotal.toFixed(2);
+	smsTotalElem.innerHTML = "R" + smsTotal.toFixed(2);
+	const totalCost = callsTotal + smsTotal;
+	totalCostElem.innerHTML = "R" + totalCost.toFixed(2);
+	totalCostElem.classList.remove('warning', 'danger');
+	if (totalCost > 50) {
+		totalCostElem.classList.add('danger');
+	} else if (totalCost > 30) {
+		totalCostElem.classList.add('warning');
+	}
+}
+addToBillBtn.addEventListener('click', textBillTotal);
 
-//add an event listener for when the add button is pressed
-
-//in the event listener check if the value in the bill type textbox is 'sms' or 'call'
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
+function resetTotals() {
+	callsTotal = 0;
+	smsTotal = 0;
+	callsTotalElem.innerHTML = "R0.00";
+	smsTotalElem.innerHTML = "R0.00";
+	totalCostElem.innerHTML = "R0.00";
+	totalCostElem.classList.remove("danger", "warning");
+}
+resetBtn.addEventListener('click', resetTotals);
